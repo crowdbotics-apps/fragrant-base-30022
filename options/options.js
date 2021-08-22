aad94d76c069acace057e99c6d9fb4a6a81d3dca
Crowdbotics <team@crowdbotics.com>
2021-08-22 13:32:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fragrant_base_30022Identifier",
+  url: "https://fragrant_base_30022Identifier.botics.co",
+  api: "https://fragrant_base_30022Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
